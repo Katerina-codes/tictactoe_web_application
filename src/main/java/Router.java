3d0ce350a10b54af90957cea88a029ae6ab40817
Katerina Georgiou <katerinaldg@gmail.com>
2018-06-27 17:59:55 +0100

    Set & store game mode for duration of game session.

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -1,5 +1,4 @@
 import game.*;
-import game.Players.PlayerFactory;
 import game.Players.WebApplicationPlayer;
 import spark.Request;
 
@@ -12,41 +11,49 @@ public class Router implements UI {
     private game.GridForWebConverter converter = new game.GridForWebConverter();
 
     public void run() {
-        get("/", (request, response) -> displayCurrentStateOfGame(new Board()));
+        get("/", (request, response) -> displayCurrentStateOfGame(new Board(), ""));
         get("/newGame/:gameMode", (request, response) -> gameMode(request));
         get("/makeMove/:move", (request, response) -> makeMoveAndUpdateBoard(request));
     }
 
     private String gameMode(Request request) {
-        return request.queryParams("gameMode");
+        String gameMode = request.queryParams("gameMode");
+        return displayCurrentStateOfGame(new Board(), gameMode);
     }
 
     private String makeMoveAndUpdateBoard(Request request) {
+        String gameMode = request.queryParams("gameMode");
         String move = request.queryParams("move");
         String currentBoard = request.queryParams("currentBoard");
         Board board = new Board(3, converter.convertToGridOfMarks(currentBoard));
         Game newGame = new Game(this, board);
+
         newGame.playerSetUp();
         WebApplicationPlayer player = (WebApplicationPlayer) newGame.currentPlayer;
         player.receiveMove(move);
         newGame.run();
-        return displayCurrentStateOfGame(board);
+        return displayCurrentStateOfGame(board, gameMode);
     }
 
-    private String displayCurrentStateOfGame(Board board) {
-        String responseBody = buildResponse(board);
+    private String displayCurrentStateOfGame(Board board, String gameMode) {
+        String responseBody = buildResponse(board, gameMode);
         return updateUIWithResult(responseBody, board);
     }
 
-    private String buildResponse(Board board) {
+    private String buildResponse(Board board, String gameMode) {
         String responseBody = "";
         responseBody += "<a href='/newGame/8?gameMode=8'> Human Vs Human </a></br>" +
                 "<a href='/newGame/9?gameMode=9'> Human Vs Unbeatable player </a><br>";
+        responseBody = buildGrid(board, responseBody, gameMode);
+        return responseBody;
+    }
+
+    private String buildGrid(Board board, String responseBody, String gameMode) {
         String convertedBoard = converter.createQueryValueForGridState(board.grid);
 
         for (int markPosition = 0; markPosition <= board.grid.size() - 1; markPosition++) {
             if (board.grid.get(markPosition).equals(Mark.EMPTY)) {
-                responseBody += String.format("<a href='/makeMove/%s?move=%s&currentBoard=%s'> %s </a>", markPosition, markPosition, convertedBoard, (markPosition + 1));
+                responseBody += String.format("<a href='/makeMove/%s?move=%s&currentBoard=%s&gameMode=%s'> %s </a>", markPosition, markPosition, convertedBoard, gameMode, (markPosition + 1));
             } else {
                 responseBody += String.format(" %s ", board.grid.get(markPosition).toString());
             }
